d252786ce50eeae34374e22a1d7d6776ee8fe137
koolaids21 <weihong2398@gmail.com>
2023-10-29 23:29:02 -0400

    zzzz

--- dom.js
+++ dom.js
@@ -1,35 +1,29 @@
+
 const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("big-black-box"),
   input: document.querySelector("#input"),
 };
 
-DOMSelectors.box.insertAdjacentHTML(
-  "beforebegin",
-  ``
-);
-
 DOMSelectors.button.addEventListener("click", function () {
   let value = DOMSelectors.input.value;
 
-  console.log(value);
+  if (value.trim() !== "") {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class='extra-text'>
+        <p>${value}</p>
+        <button class='remove-button'>Remove</button>
+      </div>
+      `
+    );
 
-  DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class='extra-text'>
-      <p>${value}</p>
-      <button class='remove-button'>Remove</button>
-    </div>
-    `
-  );
-  {
-  nameInput.value = "";
-  ageInput.value = "";
-}  {
-  alert("Please fill in both name and age fields.");
-}
-
+    DOMSelectors.input.value = ""; // Clear the input field
+  } else {
+    alert("Please fill in the input field.");
+  }
+  
   const removeButtons = document.querySelectorAll(".remove-button");
   removeButtons.forEach((removeButton) => {
     removeButton.addEventListener("click", function (event) {
